a162dde2f2527b4bdfe75e76ed88106536a5a441
zaininijar <zaininijar7610@gmail.com>
2022-10-26 08:58:15 +0700

    landing page done

---
+++ SPEEDTEST_MODULE/C1/chart.js
@@ -0,0 +1,42 @@
+class Chart {
+
+}
+
+
+let chart = new Chart({
+    title: "Data Covid Jakarta Oktober",
+    data: [
+        {
+            tanggal: 1,
+            jumlah: 13,
+        },
+        {
+            tanggal: 2,
+            jumlah: 20,
+        },
+        {
+            tanggal: 3,
+            jumlah: 70,
+        },
+        {
+            tanggal: 4,
+            jumlah: 40,
+        },
+        {
+            tanggal: 5,
+            jumlah: 60,
+        },
+        {
+            tanggal: 6,
+            jumlah: 20,
+        },
+        {
+            tanggal: 7,
+            jumlah: 50,
+        },
+        {
+            tanggal: 8,
+            jumlah: 30,
+        },
+    ]
+})
